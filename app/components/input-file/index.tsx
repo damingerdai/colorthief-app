3838ae2cddf8835c1a51c705ed096f103dd9f84f
Arthur Ming <mingguobin@live.com>
2024-09-23 15:37:32 +0800

    feat: add file upload

---
+++ app/components/input-file/index.tsx
@@ -0,0 +1,16 @@
+import { Input, InputProps } from "~/components/ui/input"
+import { Label } from "~/components/ui/label"
+
+export function InputFile(props: InputProps) {
+  return (
+    <div className="grid w-full lg:max-w-sm items-center gap-1.5">
+      <Label htmlFor="picture">Picture</Label>
+      <Input
+        id="picture"
+        type="file"
+        className="file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 file:border file:border-solid file:border-blue-700 file:rounded-md border-blue-600"
+        {...props}
+      />
+    </div>
+  )
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import { ChangeEvent, useMemo, useState } from "react";
+import { InputFile } from "~/components/input-file";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,9 +10,23 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  
+  const [file, setFile] = useState<File>();
+
+  const url = useMemo(() =>  file ? URL.createObjectURL(file) : '', [file])
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  };
+
   return (
     <div className="flex h-screen items-center justify-center">
-      <div>hello world</div>
+      <div className="mt-3">
+         <InputFile onChange={handleFileChange} accept="image/*"/>
+         { url && <img className="mt-2" width={300} src={url} alt="user upload"/>}
+      </div>
     </div>
   );
 }
